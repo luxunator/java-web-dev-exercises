f53c22977cdf3f53fd8347878fb00fa0c10b62f6
luxunator <dylan_sutcliffe@comcast.com>
2022-08-12 10:24:58 -0600

    Chapter 4 Exercises

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+public class Course {
+
+    public String name;
+    public int id;
+    public int credits;
+    private ArrayList<Student> students= new ArrayList<>();
+}
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,42 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String name, int studentId, int numberOfCredits, double gpa){
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setStudentID(String name){
+        this.studentId = studentId;
+    }
+
+    private void setNumberOfCredits(String name){
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public void setGpa(String name){
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getStudentID(){
+        return this.studentId;
+    }
+
+    private int getNumberOfCredits(){
+        return this.numberOfCredits;
+    }
+
+    public double getGpa(){
+        return this.gpa;
+    }
 }
